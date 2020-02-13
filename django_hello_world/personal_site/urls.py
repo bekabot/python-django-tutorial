23b24e71a319d147e88f89a3083d10aaf92f5823
bekabot <87282211776dom>
2020-02-14 00:59:47 +0600

    handle POST requests & generic views

--- django_hello_world/personal_site/urls.py
+++ django_hello_world/personal_site/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'personal_site'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('<int:post_title>/detail/', views.detail, name='detail'),
     path('<int:post_title>/like/', views.like, name='like'),
+    path('add/', views.add, name='add'),
 ]
--- django_hello_world/personal_site/views.py
+++ django_hello_world/personal_site/views.py
@@ -1,24 +1,37 @@
 # Create your views here.
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
 
 from .models import Post
 
 
-def index(request):
-    all_posts = Post.objects.all()
-    context = {"posts_list": all_posts}
-    return render(request, 'personal_site/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'personal_site/index.html'
+    context_object_name = 'posts_list'
+
+    def get_queryset(self):
+        return Post.objects.all()
 
 
+# should be moved to generic views as IndexView
 def detail(request, post_title):
-    try:
-        question = Post.objects.get(title=post_title)
-    except Post.DoesNotExist:
-        raise Http404("Post does not exist")
-# return render(request, 'personal_site/post.html', {'question': question})
+    post = get_object_or_404(Post, title=post_title)
+    return render(request, 'personal_site/post.html', {'post': post})
 
 
-def like(request, post_id):
-    return HttpResponse("You're liking position %s." % post_id)
+def like(request, post_title):
+    return HttpResponse("You're liking position %s." % post_title)
+
+
+def add(request):
+    # wrap following call to try/except
+    title = request.POST['title']
+    text = request.POST['text']
+
+    Post.objects.create(title=title, text=text)
+    # Always return an HttpResponseRedirect after successfully dealing
+    # with POST data. This prevents data from being posted twice if a
+    # user hits the Back button.
+    return HttpResponseRedirect(reverse('personal_site:index'))
