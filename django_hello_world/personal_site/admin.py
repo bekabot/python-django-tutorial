2133ae501adf2ea48e9640e818801eb21fd8873c
bekabot <87282211776dom>
2020-02-16 18:18:51 +0600

    static files & auto tests

--- django_hello_world/personal_site/admin.py
+++ django_hello_world/personal_site/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Post
 
+
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['text']}),
+        ('Date information', {'fields': ['title']}),
+    ]
+
+
 admin.site.register(Post)
--- django_hello_world/personal_site/tests.py
+++ django_hello_world/personal_site/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Post
+
+
+class PostModelTests(TestCase):
+    def test_db_counter(self):
+        Post.objects.create(title="test_title", text="test_text")
+        items_count = Post.objects.count()
+        self.assertIs(items_count, 1)
